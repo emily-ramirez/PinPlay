43be5d8c32254a02af8d76219318d5933797215f
Samantha Milligan <66642971+samantha-milligan@users.noreply.github.com>
2021-03-14 09:59:03 -0700

    Update views.py

--- pinplay/pinplay_api/views.py
+++ pinplay/pinplay_api/views.py
@@ -2,14 +2,26 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from pinplay_api.utils import CreatePlaylist
+from pinplay_api.utils import create_playlist
 
 
 class PlaylistView(APIView):
 
     def get(self, request):
-        '''
-        '''
-        playlist = request.query_params.get('playlist')
+    	# creates playlist using user info and playlist info
 
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    	# our API call format:
+    		# http http://127.0.0.1:8000/pinplay_api/ user_id==username auth_token==token playlist_name==name
+    		# put in your own values (username, token, name), no quotes
+
+        user_id = request.query_params.get('user_id')
+        auth_token = request.query_params.get('auth_token')
+
+        playlist_name = request.query_params.get('playlist_name')
+
+        user_info = [user_id, auth_token]
+        playlist_info = [playlist_name, None, False]
+
+        output = create_playlist(user_info, playlist_info)
+
+        return Response(output)
