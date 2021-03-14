80fa67d2c8bef55781441f78f7c0b629d5dc9aba
Samantha Milligan <66642971+samantha-milligan@users.noreply.github.com>
2021-03-14 09:57:37 -0700

    Update utils.py

--- pinplay/pinplay_api/utils.py
+++ pinplay/pinplay_api/utils.py
@@ -1,27 +1,28 @@
+import requests
+import json
 
 
-class CreatePlaylist:
-# From frontend:
-# potential display
-    # just location
-        # NYC, Phoenix, LA ( type doesnt matter )
-            # hardcoded option
+# Notes:
+    # From frontend:
+    # potential display
+        # just location
+            # NYC, Phoenix, LA ( type doesnt matter )
+                # hardcoded option
 
-    # user_ID
-    # auth_token
-    # playlist name
-    # playlist description
-    # public flag
-    # settings ( only need the actual settings, not the boolean itself )
-        # set of booleans
-            # e.g. location_flag = True
-            # settings_flags = ( public_flag, explicit_flag, weather_flag, location_flag, ... )
- 
+        # user_ID
+        # auth_token
+        # playlist name
+        # playlist description
+        # public flag
+        # settings ( only need the actual settings, not the boolean itself )
+            # set of booleans
+                # e.g. location_flag = True
+                # settings_flags = ( public_flag, explicit_flag, weather_flag, location_flag, ... )
 
-        # location ( database used here )
-            # save info, in database, on just a few cities
-        # explicit_flag
 
+            # location ( database used here )
+                # save info, in database, on just a few cities
+            # explicit_flag
 
 # NEED RESEARCH: 
     # 1. how to get user_ID and token (Spotify)
@@ -29,104 +30,137 @@ class CreatePlaylist:
     #    - https://developer.spotify.com/documentation/general/guides/authorization-guide/
     # 3. - how to call Shazam API, 
     #    - what it can return
-    #    - what format the return comes in 
-    # 4. how to get playlist_ID (Spotify)
-    # 5. how to look up songs on spotify and get song_id
-    # 6. how to add song to playlist (Spotify)
+    #    - what format the return comes in
 
-# ideas for tuples    
-user_info = ( user_ID, auth_token )
-playlist_info = ( name, description )
-
-
-# order: first
 def validate_user( user_info ):
+    pass
     # result = call on Spotify API
         # need research
     
     # return request == GOOD_HTTP_RESPONSE && auth_token == GOOD_HTTP_RESPONSE
-        
-        
+ 
+def create_playlist( user_info, playlist_info, settings_flags=None ):
+    # initially creates empty playlist and add songs to playlist
 
+    # must be in this format:
+        # user_info = [user_ID, auth_token]
+        # playlist_info = [name, description, public_flag]
 
-# order: second
-def create_playlist( user_info, playlist_info, settings_flags ):
-    # initialize variables
-        # This is the format for the spotify API to create an empty playlist
-        #  request body = {
-        #     "name": playlist_name,
-        #     "description": playlist_desc,
-        #     "public": public_flag
-        #  }
+    # call to Spotify API using user account id 
+    query = 'https://api.spotify.com/v1/users/{}/playlists'.format(user_info[0])
 
-    # responseResult = call Spotify API
-        # This function creates empty playlist
-        # we think that this will return an HTTP response, need research
+    # collects all playlist info to pass into request
+    data = json.dumps({
+        'name': playlist_info[0],
+        'description': playlist_info[1],
+        'public': playlist_info[2]
+    })
 
+    # uses token to verify authorization
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {}'.format(user_info[1])
+    }
+
+    # completes request to Spotify API
+    response = requests.post(
+        query,
+        data=data,
+        headers=headers
+    )
+
+    response = response.json()
+
+    # get playlist_id from Spotify API call
+    playlist_id = response['id']
 
     # get song list from shazam
         # code: song_list = find_songs( settings_flags )
+        # need to pass song_list into get_song_uris, currently hardcoded
+    song_list = song_list = [('Crew', 'Brent Faiyaz'), ('Si Una Vez', 'Selena')]
 
+    # search for song uri using song info
+    song_uris_list = get_song_uris(song_list, user_info)
 
-    # playlist_id = get_playlist_id(  )
+    # add all song uris to empty playlist
+    add_songs(song_uris_list, playlist_id, user_info, playlist_info)
 
+    return 'Created new playlist'
 
-    # add songs to empty playlist
-        # code: add_songs( song_list )
+def find_songs( location )
+    pass
 
-
-    # return "Playlist created"
-
-
-
-def get_playlist_id(  )
-    # need research
-        # we need to figure out how actually get the playlist_id
-
-
-
-
-# purpose: find songs that match criteria specified by user
+    # purpose: find songs that match criteria specified by user
     # for future use: we may use more parameters/criteria than location. 
     # We will need to include those values as parameters and build
     # functionality for them
-def find_songs( location )
     # need research
 
     # api call to shazam to get list of top shazammed songs for chosen
     # location
-    song_list = shazam_api_call
+    # song_list = shazam_api_call
 
 
     # may not be necessary
         # only needed if shazam API call cannot return songs based on other
         # criteria, e.g. filter out explicit songs
-    filtered_song_list = filter_song_list( settings, song_list )
+    # filtered_song_list = filter_song_list( settings, song_list )
 
     
-    return song_list
+    # return song_list
 
+def get_song_uris(song_list, user_info):
+    # collects all uris using song info and returns list
+    # song_list must be in format [(song, artist)]
 
+    # create list to add all uris
+    song_uris_list = []
 
-
-
-def add_songs( song_list, playlist_info )
-
-    position = 0
-    song_ID = None
-
-
+    # loop over all songs in song list
     for song in song_list:
+        # call to Spotify API using song name and artist
+        query = 'https://api.spotify.com/v1/search?q={},{}&type=track%2Cartist&limit=1'.format(song[0], song[1])
 
-        # call spotify API to get song info
-            # purpose: need to translate info shazam returned into info that
-            # spotify can use 
-            # code: song_id = call to spotify API( song info from shazam? )
+        # uses token to verify authorization
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {}'.format(user_info[1])
+        }
 
-        # spotify API call to add that song to the playlist
-            # appears at this moment as though we need to add every song
-            # individually
-            # Things we need: 
+        # completes request to Spotify API
+        response = requests.get(
+            query,
+            headers=headers
+        )
 
-            position++
- 
+        response = response.json()
+
+        # collect song uri
+        song_uri = response['tracks']['items'][0]['uri']
+
+        # append to song list
+        song_uris_list.append(song_uri)
+
+    return song_uris_list
+
+def add_songs( song_uris_list, playlist_id, user_info, playlist_info ):
+    # add songs to any playlist using playlist id
+
+    # call to Spotify API
+    query = 'https://api.spotify.com/v1/playlists/{}/tracks'.format(playlist_id)
+
+    # collects all song uris to pass into request
+    data = json.dumps(song_uris_list)
+
+    # uses token to verify authorization
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {}'.format(user_info[1])
+    }
+
+    # completes request to Spotify API, songs are now added to playlist
+    response = requests.post(
+        query,
+        data=data,
+        headers=headers
+    )
