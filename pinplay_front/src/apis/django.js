03e9d11b86e94f81cb3379c055930fa330e2b461
Diego de la Rosa <diegoldelarosa@gmail.com>
2021-03-25 23:30:35 -0400

    updated baseurl

--- pinplay_front/src/apis/django.js
+++ pinplay_front/src/apis/django.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 //TODO: Change the url for deployment
 export default axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://radiant-anchorage-77650.herokuapp.com/'
 });
 
