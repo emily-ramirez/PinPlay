49a4d6a5a532a01c4516d54b7ffac55220611765
kaa468 <78065060+kaa468@users.noreply.github.com>
2021-04-11 13:15:50 -0700

    UpdateShowPlaylist
    Adds containers and new positioning and look to the front page

--- pinplay_front/src/components/ShowPlaylists.js
+++ pinplay_front/src/components/ShowPlaylists.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import './../pinplay.css'
+
 const ShowPlaylists = () => {
   return(
-    <div>
-      Playlists will be displayed here in the future.
-      For now, click the button to your right to go to the playlist generation page.
-      <Link to={`/filters`} className="ui button primary">
-        <button type="button">
-          Click here!
-        </button>
-      </Link>
-    </div>
+    <div className={"container"}>
+            <div className={"jumbotron"}>
+                <h4>
+                    Playlists will be displayed here in the future.</h4>
+                <h4>
+                    For now, click the button to go to the playlist generation page.
+                </h4>
+                <div className={"ClickHere"}>
+                    <Link to={`/filters`} className="ui button primary">
+                        <button type="button" className={"ClickHere"} data-toggle={"modal"} data-target={"#myModal"}>
+                            Click here!
+                        </button>
+                    </Link></div>
+            </div>
+        </div>
   )
 }
 
